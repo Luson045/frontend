fd8473cecc885fe616a7b820e82688138bc7a90d
Luson Basumatary <131430087+Luson045@users.noreply.github.com>
2024-07-20 12:55:50 +0530

    Update getenvironment.js

--- src/getenvironment.js
+++ src/getenvironment.js
@@ -2,7 +2,7 @@
 function getEnvironment() {
   const currentURL = window.location.href;
   const development = 'http://localhost:5000';
-  const production = 'learn-stocks-7vwtdjsx2-lusons-projects.vercel.app';
+  const production = 'https://learn-stocks.onrender.com';
   if (currentURL.includes('localhost')) {
     return development;
   } else {
